fe78d4bbe6678f4449d76f55daef93256b5c7c4a
Abdul Rafey <arafey183@gmail.com>
2023-06-16 11:10:28 +0000

    Added jaffle_shop data sources

---
+++ models/dim_customers.sql
@@ -0,0 +1,45 @@
+{{
+    config(materialized="table")
+}}
+
+WITH
+
+customers AS (
+    SELECT * FROM {{ ref("stg_customer") }}
+),
+
+orders AS (
+    SELECT * FROM {{ ref("stg_order") }}
+),
+
+
+
+custom_oders AS (
+
+    SELECT
+        customer_id,
+        min(order_date) AS first_order_date,
+        max(order_date) AS most_recent_order_date,
+        count(order_id) AS number_of_orders
+    FROM
+        orders
+    GROUP BY
+        1
+),
+
+final AS (
+
+    SELECT
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        custom_oders.first_order_date,
+        custom_oders.most_recent_order_date,
+        coalesce(custom_oders.number_of_orders, 0) AS number_of_orders
+    FROM
+        customers
+    LEFT JOIN custom_oders USING (customer_id)
+)
+
+SELECT * FROM final
+
---
+++ models/staging/jaffle_shop/stg_customer.sql
@@ -0,0 +1,11 @@
+WITH customers AS (
+    
+    SELECT 
+        id AS customer_id,
+        first_name,
+        last_name
+    FROM
+        raw.jaffle_shop.customers
+)
+
+SELECT * FROM customers
